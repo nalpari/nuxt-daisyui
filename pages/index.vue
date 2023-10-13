cddd4d1aa1d5b2c4adbb13c2c86f6d83b3336397
nalpari <yoo32767@gmail.com>
2023-10-13 16:36:02 +0900

    새로운 child.vue, package.json, README.md, parent.vue, yarn.lock, [slug].vue, test.vue, index.vue, .gitignore, [id].vue, app.vue, favicon.ico, .prettierrc, nuxt.config.ts, tsconfig.json, tailwind.config.ts, .npmrc 파일을 추가했습니다.

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex justify-center h-screen space-x-4 items-center">
+    <div class="card card-compact w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img src="https://picsum.photos/400/255" alt="Image" />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions justify-end">
+          <button class="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ pages/parent.vue
@@ -0,0 +1,4 @@
+<template>
+  <p>parent</p>
+  <NuxtPage />
+</template>
